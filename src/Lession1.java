78da3db5de91775f75c1d28caacee95fe0cc4307
youngsam-byun <youngsam.byun@gmail.com>
2016-12-28 14:23:14 +0800

    practice

---
+++ src/Lession1.java
@@ -0,0 +1,31 @@
+/**
+ * Created by byun.ys on 2016-12-28.
+ */
+public class Lession1 {
+
+    public int solution(int N) {
+
+        String s=Integer.toBinaryString(N);
+
+        int max=0;
+        int count=0;
+
+        for(char c:s.toCharArray()){
+
+            if(c=='1') {
+                if(count>max)
+                    max=count;
+
+                count=0;
+                continue;
+            }
+            else
+                count++;
+
+        }
+
+        return max;
+
+    }
+
+}
