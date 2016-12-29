a4c76b3ea3bc5bf6e350cb8bd55f6259ecc1f0df
youngsam-byun <youngsam.byun@gmail.com>
2016-12-29 15:39:53 +0800

    practice

--- src/Lesson44.java
+++ src/Lesson44.java
@@ -3,17 +3,11 @@ import java.util.Map;
 
 public class Lesson44 {
 
-    public static void main(String[] args) {
-
-        solution(1,new int[]{2, 1, 1, 2, 1});
-    }
-
-    public static int[] solution(int N, int[] A) {
-        int[] counter = new int[N];
+    public  int[] solution(int N, int[] A) {
 
         int max = 0;
         int len = A.length;
-
+        int base=0;
 
         Map<Integer, Integer> hmap = new HashMap<>();
 
@@ -23,31 +17,38 @@ public class Lesson44 {
 
             if (X >= 1 && X <= N) {
 
-                counter[X - 1]++;
-
                 if (hmap.get(X - 1) == null) {
                     hmap.put(X - 1, 1);
 
-
-                    if (1 > max)
-                        max = 1;
+                    if (1+base > max)
+                        max = base+1;
 
                 } else {
+
                     int v = hmap.get(X - 1);
                     hmap.put(X - 1, v + 1);
-                    if (v + 1 > max)
-                        max = v + 1;
+
+                    if (v + 1 +base> max)
+                        max = v + 1+base;
                 }
 
             } else if (X == N + 1) {
-
-                for (int k = 0; k < N; k++)
-                    counter[k] = max;
-
+                base=max;
+                hmap.clear();
             }
-
         }
 
-        return counter;
+        int[] r = new int[N];
+        int c = 0;
+        for (int k =0;k<N;k++) {
+            int v=0;
+            if(hmap.containsKey(k))
+                v=hmap.get(k);
+
+            r[c++] = v+base;
+        }
+
+        return r;
+
     }
 }
